a9faf3d6d0b47db96a7158e9b2fbb66aa6b8cee8
KatWine <kwinegardner@pursuit.org>
2024-06-18 16:54:26 -0400

    added background color and style to main page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send('ok we did it')
+    res.send('<h1 style = background-color:green;>ok we did it<h1>')
 })
 
 app.get('/tim-gunn', (req, res) => {
@@ -12,18 +12,22 @@ app.get('/tim-gunn', (req, res) => {
 
   app.get('/Raquel', (req, res) => {
     res.send('Hire our fellows!')
+    res.send('Fail forward')
   })
 
   app.get('/Corey Kension', (req, res) => {
     res.send('I am the chosen!')
+    res.send('better scramble like eggs before you get folded like an omlet')
   }) 
   
   app.get('/Flamingo', (req, res) => {
     res.send('You dont miss the past, you miss being happy')
+   
   }) 
   
   app.get('/Saphire', (req, res) => {
     res.send('Wanna hear something scary?')
+    res.send('im your a-te saphire')
   }) 
   
   app.get('/Clown Pierce', (req, res) => {
@@ -32,6 +36,7 @@ app.get('/tim-gunn', (req, res) => {
   
   app.get('/Rengoku', (req, res) => {
     res.send('Tasty!')
+    res.send(`Stand tall and be proud, no matter how weak or unworthy you feel. Burn up your heart, grit your teeth and move forward. If you just curl up into a ball and hide, time will pass you by. It won't wait for you while you wallow in your grief.`)
   })
 
   
